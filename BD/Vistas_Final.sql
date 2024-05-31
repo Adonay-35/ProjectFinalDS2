bf5ee20f2b4db177dee62d92e26bff382003b5fd
Adonay-35 <128653790+Adonay-35@users.noreply.github.com>
2024-05-31 14:45:29 -0600

    Update Vistas_Final.sql
    Vistas_Principal

--- BD/Vistas_Final.sql
+++ BD/Vistas_Final.sql
@@ -21,10 +21,17 @@ CREATE VIEW VistaProductos AS
 SELECT 
     P.ID_Producto, 
     P.Producto, 
-    -- P.Stock, 
-    P.Precio, 
-    DATE_FORMAT(P.FechaFabricacion, '%d-%m-%Y' ) AS FechaFabricacion, 
-    DATE_FORMAT(P.FechaVencimiento, '%d-%m-%Y' ) AS FechaVencimiento, 
+    (SELECT
+        Kr.Stock
+    FROM
+        Kardex Kr
+    INNER JOIN
+        Compras Com ON Kr.ID_Compra = Com.ID_Compra
+    INNER JOIN
+        Productos Prod ON Com.ID_Producto = Prod.ID_Producto) AS Stock,
+    P.PrecioCompra, 
+    DATE_FORMAT(P.FechaFabricacion, '%d-%m-%Y') AS FechaFabricacion, 
+    DATE_FORMAT(P.FechaVencimiento, '%d-%m-%Y') AS FechaVencimiento, 
     P.Descripcion, 
     PR.Proveedor, 
     C.Categoria
@@ -36,6 +43,8 @@ INNER JOIN
     Categorias C ON P.ID_Categoria = C.ID_Categoria
 ORDER BY 
     P.Producto ASC;
+
+
     
 CREATE VIEW VistaVentas AS
 SELECT 
@@ -44,9 +53,9 @@ SELECT
     U.Usuario AS Usuario, 
     CONCAT(C.Nombres, ' ', C.Apellidos) AS Cliente, 
     P.Producto AS Producto, 
-    P.Precio,
-    V.Cantidad, 
-    V.Total
+    (P.PrecioCompra * 1.2) as Precio,
+    V.CantidadSaliente, 
+    V.TotalCobrar
 FROM 
     Ventas V
 INNER JOIN 
@@ -55,18 +64,3 @@ INNER JOIN
     Clientes C ON V.ID_Cliente = C.ID_Cliente
 INNER JOIN 
     Productos P ON V.ID_Producto = P.ID_Producto;
-    
--- Kardex
-CREATE VIEW VistaStok AS
-SELECT
-	Prod.ID_Producto,
-	Kr.Stock,
-    Prod.Producto
-FROM
-	Kardex Kr
-INNER JOIN
-    Compras Com ON Kr.ID_Compra = Com.ID_Compra
-INNER JOIN
-	Productos Prod On Com.ID_Producto = Prod.ID_Producto
-ORDER BY
-	Prod.Producto ASC;
